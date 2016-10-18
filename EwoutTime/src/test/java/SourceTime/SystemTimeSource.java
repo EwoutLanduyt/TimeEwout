362ea5f2a6feac8d4354d2669d89505f335b1df6
Ewout <Ewout@PcEwoutLanduyt>
2016-10-18 12:39:57 +0300

    classes

---
+++ EwoutTime/src/test/java/SourceTime/SystemTimeSource.java
@@ -0,0 +1,31 @@
+package SourceTime;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.net.InetAddress;
+ import java.util.Date;
+
+
+/**
+ *
+ * @author Ewout
+ */
+public class SystemTimeSource implements TimeSource {
+
+    @Override
+    public Date getTimeMoment() {
+         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       /* NTPUDPClient timeClient = new NTPUDPClient();
+        InetAddress inetAddress = InetAddress.getByName("time-a.nist.gov");
+        TimeInfo timeInfo = timeClient.getTime(inetAddress);
+        long returnTime = timeInfo.getReturnTime();
+        Date time = new Date(returnTime);
+        return time;
+*/
+    }
+
+}
